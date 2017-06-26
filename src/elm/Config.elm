6811bd83bffca34c8f76412803da10007dddacea
Rupert Smith <github@badgerhouse.freeserve.co.uk>
2017-06-26 14:01:15 +0100

    Added package docs to Config.

--- src/elm/Config.elm
+++ src/elm/Config.elm
@@ -1,9 +1,20 @@
 module Config exposing (Config, config, configDecoder)
 
+{-|
+Defines the configuration that the content editor needs to run. This provides
+urls for the services with which it interacts. A default configuration and a
+decoder for config as json are provided.
+
+@docs Config, config, configDecoder
+-}
+
 import Json.Decode as Decode exposing (..)
 import Json.Decode.Extra exposing ((|:), withDefault)
 
 
+{-|
+Defines the configuration that the content editor needs to run.
+-}
 type alias Config =
     { applicationContextRoot : String
     , apiRoot : String
@@ -11,6 +22,9 @@ type alias Config =
     }
 
 
+{-|
+Provides a default configuration.
+-}
 config : Config
 config =
     { applicationContextRoot = "/jtrial/"
@@ -19,6 +33,9 @@ config =
     }
 
 
+{-|
+Implements a decoder for the config as json.
+-}
 configDecoder : Decoder Config
 configDecoder =
     (Decode.succeed
