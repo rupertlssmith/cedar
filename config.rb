b0ffd3bf3cf0437430c92082997d2ca0ffbe88a6
Rupert Smith <rupert@thesett.com>
2018-10-31 11:18:45 +0000

    Tidied away the old build.

--- config.rb
+++
@@ -1,8 +0,0 @@
-require 'compass/import-once/activate'
-
-http_path = "/"
-css_dir = "app/styles"
-sass_dir = "src/styles"
-#images_dir = "images"
-#javascripts_dir = "javascripts"
-#generated_images_dir = "genimage"
